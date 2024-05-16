0913c7785a9dcd8dda5a9a3b27035421dbec280f
Maxim Belyak <maxxim.belyak.228@gmail.com>
2024-05-16 12:39:18 +0300

    Add project files.

---
+++ HelloApp/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace HelloApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
